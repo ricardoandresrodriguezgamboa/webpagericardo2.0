91bbd226f4545415b0669e35ec3c13f661375e1f
Ricardo Rodriguez <ricardoandres749@gmail.com>
2017-02-18 16:14:17 -0300

    fix responsive errors

--- js/menu.js
+++ js/menu.js
@@ -6,9 +6,11 @@ $(function(){
         var headroom  = new Headroom(header);
         
         headroom.init(); */
+         
+    
 
     
- (function($) {
+ /*(function($) {
      
         $.fn.visible = function(partial) {
             
@@ -49,7 +51,49 @@ $(function(){
         });
         });
 
-        
+*/
+
 });
+
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('#navegacion-rr ul li a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 2000, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#navegacion-rr a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#navegacion-rr ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
 
 
