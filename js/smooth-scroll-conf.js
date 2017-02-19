27b68c0edc78c5a10d89592e41805fa4f62fa9d6
Ricardo Rodriguez <ricardoandres749@gmail.com>
2017-02-19 13:36:16 -0300

    clean letters menu

--- js/smooth-scroll-conf.js
+++ js/smooth-scroll-conf.js
@@ -1,8 +1,8 @@
 smoothScroll.init({
-    selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
-    selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
-    speed: 2000, // Integer. How fast to complete the scroll in milliseconds
-    easing: 'easeInOutCubic', // Easing pattern to use
-    offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
-    callback: function ( anchor, toggle ) {} // Function to run after scrolling
+    selector: '[data-scroll]',
+    selectorHeader: null, 
+    speed: 2000, 
+    easing: 'easeInOutCubic', 
+    offset: 0, 
+    callback: function ( anchor, toggle ) {} 
 });
